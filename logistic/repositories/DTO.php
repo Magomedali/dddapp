eac99c617b97a3d879ebb81ea4d2da6ceeb5f1a2
Magomedali <web-ali@yandex.ru>
2019-02-26 01:23:03 +0300

    struct

---
+++ logistic/repositories/DTO.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace logist\repositories;
+
+/**
+ * Description of DTO
+ *
+ * @author ali
+ */
+class DTO {
+    //put your code here
+}
